c97b73464d8dd56cf6ecabaea114825d87421250
muhammadfauzi5612-art <muhammadfauzi5612@gmail.com>
2025-10-08 20:35:49 +0700

    Day 2

---
+++ bin/Dart Dasar/MenggunakanForIn.dart
@@ -0,0 +1,7 @@
+void main() {
+  var array = <String>['Namaku', 'Muahmmad', 'Fauzi'];
+
+  for (var value in array) {
+    print(value);
+  }
+}
---
+++ bin/Dart Dasar/TanpaForIn.dart
@@ -0,0 +1,7 @@
+void main() {
+  var array = <String>['Namaku', 'Muhammad', 'Fauzi'];
+
+  for (var i = 0; i < array.length; i++) {
+    print(array[i]);
+  }
+}
